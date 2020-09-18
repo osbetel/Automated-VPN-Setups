8ec431116243947b4a063a7c795386ddfe7cf497
Andrew Nguyen <osbetel@gmail.com>
2020-09-17 22:51:56 -0700

    update of ikev2 vpn

--- configure-ikev2.py
+++ configure-ikev2.py
@@ -7,6 +7,7 @@ os.system("pip3 install requests")
 # Save public server IP for later
 ip = requests.get("http://ifconfig.me").content.decode("utf-8")
 name = socket.gethostname() + " – " + ip
+exclude = "EXCLUDED_SSID"
 
 # find the name in the generate-mobileconfig file and edit it before the dockerfile is built.
 # : ${PROFILE_NAME="IKEv2 VPN Profile"}
@@ -25,13 +26,9 @@ genConfig.close()
 
 # Build and run docker container for ikev2
 os.system("docker build docker-ikev2 -t docker-ikev2")
-os.system("docker run --privileged -d --name docker-ikev2 "
-          "--restart=always -p 500:500/udp -p 4500:4500/udp docker-ikev2")
+os.system(f"""docker run --cap-add=NET_ADMIN -d --name docker-ikev2 --restart=always -p 500:500/udp -p 4500:4500/udp -e "HOST={ip}" -e "HOSTNAME={name}" -e "EXCLUDE_SSID={exclude}" docker-ikev2""")
 # unfortunately ikev2 protocol runs on ports 500 and 4500. Port 500 for data, port 4500
 # as the IPSec control path (ikev2 wraps key exchange inside an IPSec tunnel)
 
 # Generating the mobileconfig file and output to directory
-os.system("docker run --privileged -i -t --rm --volumes-from "
-          "docker-ikev2 -e \"HOST=" + ip + "\" docker-ikev2 generate-mobileconfig > ikev2.mobileconfig")
-
-
+os.system("docker exec -it docker-ikev2 generate-mobileconfig > ikev2.mobileconfig")
