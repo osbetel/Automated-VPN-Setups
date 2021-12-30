ced826f5fb6d94b70db0c74862f83a4aa6e02aae
Andrew Nguyen <osbetel@gmail.com>
2021-12-30 15:18:25 -0800

    update

--- configure-ikev2.py
+++ configure-ikev2.py
@@ -7,7 +7,7 @@ os.system("pip3 install requests")
 # Save public server IP for later
 ip = requests.get("http://ifconfig.me").content.decode("utf-8")
 name = socket.gethostname() + " – " + ip
-exclude = "%032x" % random.getrandbits(128)
+exclude = "Mexico"
 
 # find the name in the generate-mobileconfig file and edit it before the dockerfile is built.
 # : ${PROFILE_NAME="IKEv2 VPN Profile"}
